f00841915627f18867f2e98021ad82534a4b432c
Gatti <eduardo.gf@hotmail.com>
2021-04-15 21:23:46 -0400

    SMTP

--- forms/contact.php
+++ forms/contact.php
@@ -18,10 +18,10 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'imap.secureserver.net',
+    'host' => 'smtpout.secureserver.net',
     'username' => 'leandro@pirescarpentryinc.com',
     'password' => '34211694Leandro',
-    'port' => '993'
+    'port' => '465'
   );
   
 
